555c11a62069fca890bf4b52a26f2f8597514a2a
Mevra Dilan Erten Özbunar <112767383+mevradilanozbunar@users.noreply.github.com>
2023-03-02 03:55:59 +0300

    crud operations

--- controllers/contactController.js
+++ controllers/contactController.js
@@ -28,21 +28,42 @@ const createContact = asyncHandler (async(req,res)=> {
 //@route GET /api/contacts/:id
 //@access public
 const getContact =asyncHandler (async (req,res)=> {
-    res.status(200).json({message:`get contact for ${req.params.id}`});
+  const contact =await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+    res.status(200).json(contact);
   });
 
 //@desc Update contact
 //@route PUT /api/contacts/:id
 //@access public
 const updateContact = asyncHandler (async(req,res)=> {
-  res.status(200).json({message:`update contact for ${req.params.id}`});
+  const contact =await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  const updatedContact=await Contact.findByIdAndUpdate(
+    req.params.id,
+    req.body,
+    {new:true}
+  );
+  res.status(200).json(updatedContact);
 });
 
 //@desc Delete contact
 //@route DELETE /api/contacts/:id
 //@access public
 const deleteContact=asyncHandler (async(req,res)=> {
-  res.status(200).json({message:`delete contact for ${req.params.id}`});
+  const contact =await Contact.findById(req.params.id);
+  if (!contact) {
+    res.status(404);
+    throw new Error("Contact not found");
+  }
+  await Contact.deleteOne();
+  res.status(200).json(contact);
 });
 
 
